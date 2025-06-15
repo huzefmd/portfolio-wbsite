928adc915513074aea08e4c09dc4d0eb65ca8381
huzefmd <huzefmd490@gmail.com>
2025-06-16 00:25:52 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className='bg-[#000000] text-white'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
